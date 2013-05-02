c393f5e20bd0189405381b168054d7b68ed8fd7f
Paul Kilgo <paulkilgo@gmail.com>
2013-05-01 23:01:58 -0400

    add some other useful functions

---
+++ pylib/synth/trig.py
@@ -0,0 +1,25 @@
+import math
+from synth.channel import Channel
+
+class Sin(Channel):
+  def __call__(self, t):
+    return math.sin(2 * math.pi * t)
+
+class Cos(Channel):
+  def __call__(self, t):
+    return math.cos(2 * math.pi * t)
+
+class Tan(Channel):
+  def __call__(self, t):
+    return math.tan(2 * math.pi * t)
+
+class Sawtooth(Channel):
+  def __call__(self, t):
+    return -1 + 2 * (t - math.floor(t))
+
+class Square(Channel):
+  def __init__(self, duty=0.5, *args, **kwargs):
+    self.duty = duty
+
+  def __call__(self, t):
+    return 1.0 if (t - math.floor(t)) < self.duty else -1.0
